b55c7625c483b80922246d2bd4b91ffa777f25d5
TheWisePigeon <zozozozeph@gmail.com>
2022-08-07 23:56:16 +0000

    testing sum

--- api/store_sub.js
+++ api/store_sub.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const subs = require('../public/subs.json')
+
 export default function handler( _req, res){
-    return res.send('Hello World');
+    console.log(subs);
+    return res.send('Hello World', subs.subs.addresses);
 }
