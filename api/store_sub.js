2c6697526055385d31a25a24e28f20b87fc09cbd
TheWisePigeon <zozozozeph@gmail.com>
2022-08-08 01:40:56 +0000

    fixed cors issue4

--- api/store_sub.js
+++ api/store_sub.js
@@ -1,7 +1,24 @@
 const fs = require('fs')
 const subs = require('./subs.json')
 
-export default function handler(req, res) {
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+}
+
+async function handler(req, res) {
     const { newSub } = req.body
     subs.subs.addresses.push(newSub)
     fs.writeFile('./api/subs.json', JSON.stringify(subs), function writeJSON(err) {
@@ -12,3 +29,5 @@ export default function handler(req, res) {
         "status": "success"
     });
 }
+
+export default allowCors(handler)
