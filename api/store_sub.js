0a4b3d24b4312192362f93f61d71b4519531adf7
TheWisePigeon <zozozozeph@gmail.com>
2022-08-08 00:30:36 +0000

    added function to store subbed users

--- api/store_sub.js
+++ api/store_sub.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 const subs = require('./subs.json')
 
-export default function handler( _req, res){
-    return res.send(subs);
+export default function handler(req, res) {
+    const { newSub } = req.body
+    subs.subs.addresses.push(newSub)
+    fs.writeFile('./api/subs.json', JSON.stringify(subs), function writeJSON(err) {
+        if (err) return console.log(err)
+    });
+    return res.send('200');
 }
--- api/test.js
+++ api/test.js
@@ -1,2 +1,8 @@
 const subs = require('./subs.json')
+const fs = require('fs')
 console.log(subs);
+const newSub = 'huruh'
+subs.subs.addresses.push(newSub)
+fs.writeFile('./api/subs.json', JSON.stringify(subs), function writeJSON(err) {
+    if (err) return console.log(err)
+});
